912b4088ca16a79ffeb60a69e25b6432b9b882ab
Bruno Parziovanni <70079144+Kiny-Kiny@users.noreply.github.com>
2021-12-07 12:47:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from requests import get
 
 for i in get({1:'http://cnes.datasus.gov.br/services/profissionais?nome=CARLOS%20ALBERTO',2:'http://cnes.datasus.gov.br/services/profissionais?cpf=00000000272'}[1], headers={"User-Agent": "Mozilla/5.0 (Android 10; Mobile; rv:94.0) Gecko/94.0 Firefox/94.0","Referer": "http://cnes.datasus.gov.br/pages/profissionais/consulta.jsp"}).json():
     for k,v in i.items():
-        print(k,':',v,'\n')
+        print(k,':',v)
     print('--------------------')
